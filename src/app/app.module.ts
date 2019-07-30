8ba39a568dc583e64cd07b6ae9ed23fec479b7e6
eneylton <eneylton@hotmail.com>
2019-07-29 23:37:13 -0300

    Funcionando fileupload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,26 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { File } from '@ionic-native/file';
+import { Camera } from '@ionic-native/camera';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+
+import firebase from 'firebase';
+
+var config ={
+  apiKey: "AIzaSyCllAzGpjGLCcv8U16WKzV9FiJKHJFDpiE",
+  authDomain: "pueraza-store-web.firebaseapp.com",
+  databaseURL: "https://pueraza-store-web.firebaseio.com",
+  projectId: "pueraza-store-web",
+  storageBucket: "pueraza-store-web.appspot.com",
+  messagingSenderId: "1009451131611",
+  appId: "1:1009451131611:web:b646280833525d24"
+}
+
+firebase.initializeApp(config);
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +38,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config), 
+    AngularFireStorageModule 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,6 +50,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    File,
+    Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { File } from '@ionic-native/file';
+import { AngularFireStorage} from '@angular/fire/storage';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+
+
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +15,57 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public uploadPercent:Observable<number>;
+  public downloadUrl:Observable<string>;
+   
 
+  constructor(public navCtrl: NavController,
+    private file: File,
+    private afStorage: AngularFireStorage,
+    private camera: Camera) {}
+
+  async openGaleria(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      correctOrientation:true
+    };
+    try{
+
+      const fileUri: string= await this.camera.getPicture(options);
+
+      let file: string;
+
+      file = fileUri.substring(fileUri.lastIndexOf('/') + 1, fileUri.indexOf('?'));
+
+      const path:string = fileUri.substring(0,fileUri.lastIndexOf('/'));
+
+      console.log(path);
+
+      const buffer:ArrayBuffer = await this.file.readAsArrayBuffer(path,file);
+
+      const blob = new Blob([buffer],{type:"image/jpeg"});
+      
+      console.log(blob);
+
+      this.uploadImagem(blob);
+
+    }catch(error){
+     console.log(error);
+    }
+  }
+
+  uploadImagem(blob: Blob){
+   const ref = this.afStorage.ref('imagem.jpg');
+   const task = ref.put(blob);
+
+   this.uploadPercent = task.percentageChanges();
+  
+   task.snapshotChanges().pipe(
+     finalize(()=> this.downloadUrl = ref.getDownloadURL()
+     )).subscribe();
+  
   }
 
 }
